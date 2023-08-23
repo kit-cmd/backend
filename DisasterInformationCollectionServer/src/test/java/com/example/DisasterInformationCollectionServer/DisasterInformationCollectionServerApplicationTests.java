9c2279321fc009055ecf2753410e5c2d8d3425d5
freemoon99 <soloist132@gmail.com>
2023-08-23 17:52:09 +0900

    fix: 테스트 코드 수정

--- DisasterInformationCollectionServer/src/test/java/com/example/DisasterInformationCollectionServer/DisasterInformationCollectionServerApplicationTests.java
+++ DisasterInformationCollectionServer/src/test/java/com/example/DisasterInformationCollectionServer/DisasterInformationCollectionServerApplicationTests.java
@@ -1,8 +1,11 @@
+package com.example.DisasterInformationCollectionServer;
+
 import com.example.DisasterInformationCollectionServer.service.DisasterDataService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
