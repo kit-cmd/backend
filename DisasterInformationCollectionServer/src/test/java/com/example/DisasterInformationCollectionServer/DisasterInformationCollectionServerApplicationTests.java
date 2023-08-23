5f0790d4657b00cff56d45c0e2b3d967e550c869
freemoon99 <soloist132@gmail.com>
2023-08-23 17:30:22 +0900

    fix: 테스트코드 수정

--- DisasterInformationCollectionServer/src/test/java/com/example/DisasterInformationCollectionServer/DisasterInformationCollectionServerApplicationTests.java
+++ DisasterInformationCollectionServer/src/test/java/com/example/DisasterInformationCollectionServer/DisasterInformationCollectionServerApplicationTests.java
@@ -1,18 +1,26 @@
 package com.example.DisasterInformationCollectionServer;
 
-import com.example.DisasterInformationCollectionServer.service.TestDataProvider;
+import com.example.DisasterInformationCollectionServer.service.DisasterDataService;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DisasterInformationCollectionServerApplicationTests {
+
+	private final DisasterDataService disasterDataService; // 생성자 주입
+
+	public DisasterInformationCollectionServerApplicationTests(DisasterDataService disasterDataService) {
+		this.disasterDataService = disasterDataService;
+	}
 
 	@Test
 	public void testGetValueFromRedis() {
 		String key = "myKey";
 		String expectedValue = "myValue";
 
-		String actualValue = DisasterDataService.getValueFromRedis(key); // 수정 필요
+		String actualValue = disasterDataService.getValueFromRedis(key); // 수정 필요
 
 		assertEquals(expectedValue, actualValue);
 	}
